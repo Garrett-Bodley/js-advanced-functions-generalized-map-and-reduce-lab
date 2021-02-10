272bd70b942176479f034d139dab13b9eb6081d7
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-02-09 23:30:07 -0500

    Finished lab.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your functions here
+function map(array, block){
+  let result = [];
+  array.forEach(el => result.push(block(el)));
+  return result
+};
+
+// function reduce(array, block, starting){
+//   return starting != undefined ? array.reduce(block, starting) : array.reduce(block)
+// };
+
+function reduce(array, block, starting){
+  let memo = !!starting ? starting : array[0];
+  let i = !!starting ? 0 : 1;
+
+  for (; i < array.length; i++){
+    memo = block(array[i], memo)
+  };
+  return memo;
+}
+
